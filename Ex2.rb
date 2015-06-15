97e66ba71937b2ea242b8d5d18c04fbf77db14df
Jerry Khalil <email.jerry@yahoo.com>
2015-06-15 11:26:56 -0500

    Workbook Quiz 2 Exercises

---
+++ Ex2.rb
@@ -0,0 +1,4 @@
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237}
+total_years = 0
+ages.each {|name,age| total_years +=age} 
+puts total_years
